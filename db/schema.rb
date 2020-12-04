894f6789da874e93dc2d807c27d7c1cd1e9f7dfe
tsuyoshi-ueda-ri <63494238+tsuyoshi-ueda-ri@users.noreply.github.com>
2020-12-04 22:41:40 +0900

    My page implementation
    ユーザーマイページ実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_032012) do
+ActiveRecord::Schema.define(version: 2020_12_04_134014) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_12_04_032012) do
     t.text "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
